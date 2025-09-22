97b0ea5e802762e1ac26bafe164470c89a072b15
Fabian Netz <fabian.netz@outlook.de>
2025-09-22 16:02:15 +0200

    new

--- updater.py
+++ updater.py
@@ -4,13 +4,35 @@ import dotenv
 
 logger = logging.getLogger(__name__)
 client = docker.from_env()
+logging.basicConfig(filename='updater.log', level=logging.INFO)
 
-def app_logging():
-    logging.basicConfig(filename='updater.log', level=logging.INFO)
-    container = client.images.list()
-    
-    print(container)
-    
+def fetch_images():
+    container_id_list = []
+    list_of_images = []
 
+    for container in client.containers.list():
+        container_id_list.append(container.id)
+        
+
+    for image in container_id_list:
+        conti = client.containers.get(image)
+        images = conti.attrs['Config']['Image']
+        list_of_images.append(images)
+    
+    return container_id_list
+
+def get_new_image_id():
+    list_of_images = fetch_images()
+    conti = client.containers.get(list_of_images[2])
+    images = conti.id
+    print(client.images.get_registry_data(images))
+    print(images)
+
+
+def delete_unused_images():
+    image_prune = client.images.prune()
+    print(image_prune)
+    
 if __name__ == '__main__':
-    app_logging()
+    fetch_images()
+    get_new_image_id()
