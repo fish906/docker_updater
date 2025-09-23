2ef8923463c0a44a66f9866e8b168581c6d425fb
Fabian Netz <fabian.netz@outlook.de>
2025-09-23 18:25:50 +0200

    work in progress

--- updater.py
+++ updater.py
@@ -17,27 +17,41 @@ def fetch_container_images():
     return container_images
 
 def get_local_digest():
-    container_images = fetch_container_images()
-
-    for image_name in container_images:
-        local_image = client.images.get(image_name)
-        local_digest = local_image.attrs.get("RepoDigests", [None])[0]
-
-        return local_digest
-
-container_image = fetch_container_images()
-
-def get_local_digest():
     local_digest_list = []
 
-    logger.info('Extracting local digest...')
-    for image_name in container_image:
+    for image_name in container_images:
         local_image = client.images.get(image_name)
         local_digest = local_image.attrs.get("RepoDigests", [None])[0]
         local_digest_list.append(local_digest)
 
     return local_digest_list
-    
+
+def get_remote_digest():
+    remote_digest_list = []
+
+    try:
+        for image_name in container_images:
+            new_image = client.images.pull(image_name)
+            new_digest = new_image.attrs.get("RepoDigests", [None])[0]
+            remote_digest_list.append(new_digest)
+        
+    except Exception as e:
+        print(e)
+
+    return remote_digest_list
+        
+
+def check_for_update():
+    for i in range(0, len(ld) - 1):
+        if ld[i] == rd[i]:
+            print(f'Image is up to date: {ld[i]}')
+
+        else:
+            print('Update is available...')
+
 if __name__ == '__main__':
-    fetch_container_images()
+    container_images = fetch_container_images()
+    ld = get_local_digest()
+    rd = get_remote_digest()
+
     check_for_update()
